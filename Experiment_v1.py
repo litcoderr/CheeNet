290dd17d719f03c325de109685729eb289d1ae1e
James Chee <litcoderr@gmail.com>
2018-08-23 17:14:59 +0900

    Added Descriptions

--- Experiment_v1.py
+++ Experiment_v1.py
@@ -7,11 +7,20 @@ You are welcome to contribute!!
 import CheeNet_v1 as CheeNet
 
 if __name__ == '__main__':
-    NN = CheeNet.NeuralNet(learning_rate=0.1,epochs=4000,limit=0.1)
-    NN.add_trainingSet([[0,1],[2,3]],[[1,0],[0,1]])
+    # Configure Neural Net
+    NN = CheeNet.NeuralNet(learning_rate=0.1,epochs=10000,limit=0.1)
+    NN.add_trainingSet([[0,0],[1,0],[0,1],[1,1]],[[1,0],[0,1],[0,1],[1,0]])
+
+    # Make the Architecture of the Network
     NN.add_Layer('sigmoid',2,3)
+    NN.add_Layer('sigmoid',3,4)
+    NN.add_Layer('sigmoid',4,3)
     NN.add_Layer('sigmoid',3,2)
     NN.add_Layer('softmax',2,1)
+    
+    # Train the Network
     NN.train()
-    NN.feedforward([2,3])
+
+    # Test
+    NN.feedforward([0,0])
     NN.print_Result()
